3516616b143581a7f229afaee9b01c0d8ec87d9b
Glen Hayoge <glenhayoge@gmail.com>
2024-02-08 12:48:42 +1000

    added daisyui and fixed nav comps

---
+++ src/data/nav.ts
@@ -0,0 +1,19 @@
+export const nav = [
+    {
+        title: 'Home',
+        slug: '/',
+      },
+    {
+        title: 'About',
+        slug: '/about',
+      },
+    {
+      title: 'Blog',
+      slug: '/blog',
+    },
+    {
+        title: 'Contact',
+        slug: '/contact',
+      },
+   
+  ]
